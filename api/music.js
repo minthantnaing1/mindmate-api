e65b6b8722c05edd875ffebbac19e7cc27b68171
Min Thant Naing <u6530233@au.edu>
2025-02-22 11:17:18 +0700

    Update Music API 0.2

--- api/music.js
+++ api/music.js
@@ -9,27 +9,27 @@ const musicData = [
     {
         id: 1,
         title: "Baptized in Fear",
-        url: "https://drive.google.com/file/d/1S5gfFebPO_GsSVEuj3G3PaEHmhvAywxk/view?usp=drive_link"
+        url: "https://drive.google.com/uc?export=download&id=1S5gfFebPO_GsSVEuj3G3PaEHmhvAywxk"
     },
     {
         id: 2,
         title: "Open Hearts",
-        url: "https://drive.google.com/file/d/1qagb3TVjQ8AVKptUvJEvb0FVEpf0JnuZ/view?usp=drive_link"
+        url: "https://drive.google.com/uc?export=download&id=1qagb3TVjQ8AVKptUvJEvb0FVEpf0JnuZ"
     },
     {
         id: 3,
         title: "Enjoy The Show",
-        url: "https://drive.google.com/file/d/1bpi0kpmEf8YF4c7EpLc8abI26uubWLA2/view?usp=drive_link"
+        url: "https://drive.google.com/uc?export=download&id=1bpi0kpmEf8YF4c7EpLc8abI26uubWLA2"
     },
     {
         id: 4,
         title: "Niagara Falls",
-        url: "https://drive.google.com/file/d/1ZC28Jp_elKGmrZ7NPhCfKqKgckxVolh-/view?usp=drive_link"
+        url: "https://drive.google.com/uc?export=download&id=1ZC28Jp_elKGmrZ7NPhCfKqKgckxVolh-"
     },
     {
         id: 5,
         title: "Take Me Back to LA",
-        url: "https://drive.google.com/file/d/1Pya76nMGwTcD2Dw4t1rVVAYemIXM7bPI/view?usp=drive_link"
+        url: "https://drive.google.com/uc?export=download&id=1Pya76nMGwTcD2Dw4t1rVVAYemIXM7bPI"
     }
 ];
 
